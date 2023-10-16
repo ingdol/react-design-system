7a0c5c16deeb7ebfc801f49621d17443751bbecc
ingdol <dmswl3073@naver.com>
2023-10-16 16:30:38 +0900

    [fix] header 고정 추가 및 sub header 추가 중, slider 컴포넌트 완료

---
+++ src/SimpleSlider.jsx
@@ -0,0 +1,15 @@
+import Slider from 'react-slick';
+
+export const SimpleSlider = ({ children }) => {
+  const settings = {
+    dots: false,
+    infinite: true, // 마지막 슬라이드 다음에 처음으로 돌아가기
+    // autoplay: true, // 자동 넘기기
+    // autoplaySpeed: 3000,
+    pauseOnHover: true, // 슬라이드 위에 올렸을때 멈춤
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+  return <Slider {...settings}>{children}</Slider>;
+};
